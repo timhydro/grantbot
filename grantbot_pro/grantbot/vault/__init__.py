9076352772870aa700f4a6af504790f76c4709e9
timhydro <144577357+timhydro@users.noreply.github.com>
2026-08-17 17:26:17 -0500

    Wire modular document vault package exports

--- grantbot_pro/grantbot/vault/__init__.py
+++ grantbot_pro/grantbot/vault/__init__.py
@@ -1,18 +1,20 @@
 """Controlled, versioned organizational document vault for GrantBot."""
 
-from grantbot.vault.service import (
+from grantbot.vault.packet import attach_document_to_packet
+from grantbot.vault.readiness import readiness_dashboard
+from grantbot.vault.repository import (
     add_requirement,
     approve_document,
-    attach_document_to_packet,
     document_freshness,
     get_document,
     list_documents,
+    list_events,
     list_links,
     list_requirements,
     link_document,
-    readiness_dashboard,
     resolve_document_path,
     review_document,
+    set_requirement_active,
     store_document,
 )
 
@@ -23,11 +25,13 @@ __all__ = [
     "document_freshness",
     "get_document",
     "list_documents",
+    "list_events",
     "list_links",
     "list_requirements",
     "link_document",
     "readiness_dashboard",
     "resolve_document_path",
     "review_document",
+    "set_requirement_active",
     "store_document",
 ]
