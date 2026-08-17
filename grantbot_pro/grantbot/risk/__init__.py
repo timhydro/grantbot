2d9a31d096d3226c5881ec06b8f0e91615fe81ab
timhydro <144577357+timhydro@users.noreply.github.com>
2026-08-17 08:30:01 -0500

    Add risk intelligence package

---
+++ grantbot_pro/grantbot/risk/__init__.py
@@ -0,0 +1,11 @@
+from grantbot.risk.engine import (
+    RiskAssessment,
+    assess_workspace_risk,
+    risk_history,
+)
+
+__all__ = [
+    "RiskAssessment",
+    "assess_workspace_risk",
+    "risk_history",
+]
