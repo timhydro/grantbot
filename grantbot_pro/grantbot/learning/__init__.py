77c80adc5845d0562675058b35906eebe358d833
timhydro <144577357+timhydro@users.noreply.github.com>
2026-08-17 08:44:24 -0500

    Add adaptive review learning package

---
+++ grantbot_pro/grantbot/learning/__init__.py
@@ -0,0 +1,15 @@
+from grantbot.learning.review_learning import (
+    DIMENSION_WEIGHTS,
+    approved_examples,
+    learning_profile,
+    review_history,
+    submit_review,
+)
+
+__all__ = [
+    "DIMENSION_WEIGHTS",
+    "approved_examples",
+    "learning_profile",
+    "review_history",
+    "submit_review",
+]
