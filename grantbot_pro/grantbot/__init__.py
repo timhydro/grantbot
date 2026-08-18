3207a80cf83e1df7098843ae7e708040785a06a8
timhydro <144577357+timhydro@users.noreply.github.com>
2026-08-18 03:23:43 -0500

    Bump GrantBot to v21.0.0

--- grantbot_pro/grantbot/__init__.py
+++ grantbot_pro/grantbot/__init__.py
@@ -3,6 +3,6 @@ GrantBot Pro
 BrokenGrowthMinistries Funding Intelligence Platform
 """
 
-__version__ = "20.9.0"
+__version__ = "21.0.0"
 __app_name__ = "GrantBot Pro"
 __organization__ = "BrokenGrowthMinistries"
