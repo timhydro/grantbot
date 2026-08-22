23e0a3978dd2a12ca0ea37fa2d36e48284f683e4
timhydro <144577357+timhydro@users.noreply.github.com>
2026-08-21 22:00:09 -0500

    feat(v28): mount reusable multi-model intelligence API

--- grantbot_pro/grantbot/app.py
+++ grantbot_pro/grantbot/app.py
@@ -85,6 +85,7 @@ from grantbot.api.funding_live_v24 import router as funding_live_v24_router
 from grantbot.api.agentic_v26 import router as agentic_v26_router
 from grantbot.api.workflow_v27 import router as workflow_v27_router
 from grantbot.api.irs1023_v28 import router as irs1023_v28_router
+from grantbot.api.multimodel_v28 import router as multimodel_v28_router
 
 app.include_router(master_pipeline_v5_router)
 app.include_router(discovery_v6_router)
@@ -118,3 +119,4 @@ app.include_router(funding_live_v24_router)
 app.include_router(agentic_v26_router)
 app.include_router(workflow_v27_router)
 app.include_router(irs1023_v28_router)
+app.include_router(multimodel_v28_router)
