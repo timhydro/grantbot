8a0a5bbf86c1d3af23e9f3de428a20072a7be66d
timhydro <144577357+timhydro@users.noreply.github.com>
2026-08-21 21:43:24 -0500

    Align GrantBot package version to 27.2.0

--- grantbot_pro/grantbot/__init__.py
+++ grantbot_pro/grantbot/__init__.py
@@ -3,6 +3,6 @@ GrantBot Pro
 BrokenGrowthMinistries Funding Intelligence Platform
 """
 
-__version__ = "27.1.0"
+__version__ = "27.2.0"
 __app_name__ = "GrantBot Pro"
 __organization__ = "BrokenGrowthMinistries"
