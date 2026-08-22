9d0d56b8645ea72b207ae390c3f051071da9e14e
timhydro <144577357+timhydro@users.noreply.github.com>
2026-08-21 23:09:54 -0500

    chore(v30): bump GrantBot package version

--- grantbot_pro/grantbot/__init__.py
+++ grantbot_pro/grantbot/__init__.py
@@ -3,6 +3,6 @@ GrantBot Pro
 BrokenGrowthMinistries Funding Intelligence Platform
 """
 
-__version__ = "29.0.0"
+__version__ = "30.0.0"
 __app_name__ = "GrantBot Pro"
 __organization__ = "BrokenGrowthMinistries"
